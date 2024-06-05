4904122f373276acb7f6def57619a491faee9264
genny2010 <genny.rivera@utp.edu.co>
2024-06-05 14:46:55 -0500

    Fix in migrations

--- EasyPermissions/EasyPermissions.Backend/Migrations/20240605192143_add entities.Designer.cs
+++ EasyPermissions/EasyPermissions.Backend/Migrations/20240605192143_add entities.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EasyPermissions.Backend.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20240601215314_Add permission entity")]
-    partial class Addpermissionentity
+    [Migration("20240605192143_add entities")]
+    partial class addentities
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
